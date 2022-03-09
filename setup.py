27548b55035b1af0311a6d66cbcb93b0832c54c8
Scalia <brunoleite@suzano.com.br>
2022-03-09 20:31:20 -0300

    setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+  name = 'collocation',
+  packages = ['collocation'],
+  version = '0.1.0',
+  license='MIT',
+  description = 'A repository with a python implementation of Orthogonal Collocation by Villadsen and Stewart (1967) for solving second order boundary value problems.',
+  author = 'Bruno Scalia C. F. Leite',
+  author_email = 'bruscalia12@gmail.com',
+  url = 'https://github.com/bruscalia/collocation',
+  download_url = 'https://github.com/bruscalia/collocation',
+  keywords = ['Orthogonal Collocation',
+              'Boundary Value Problems',
+              'Differential Equations',
+              'Numerical Methods'],
+  install_requires=[
+          'numpy==1.19.*',
+          'scipy==1.7.*',
+          'math',
+      ],
+)
